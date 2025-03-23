c77e2e29e4ed8be2714dd85ca91ea7c35c2421df
Ryan Le <ryanvle@uw.edu>
2025-03-22 17:36:01 -0700

    Quartz sync: Mar 22, 2025, 5:36 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -10,8 +10,7 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/ryanvle/ryanQuartz",
     },
   }),
 }
